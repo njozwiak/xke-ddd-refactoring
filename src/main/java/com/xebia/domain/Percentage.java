0037c6036e5110cad61d4983d7ad1d65e0ea3e65
njozwiak <njozwiak@xebia.fr>
2013-12-12 13:05:46 +0100

    Add Percentage

---
+++ src/main/java/com/xebia/domain/Percentage.java
@@ -0,0 +1,13 @@
+package com.xebia.domain;
+
+import java.math.BigDecimal;
+
+public class Percentage {
+
+    private BigDecimal value;
+
+    public Percentage(BigDecimal value) {
+        this.value = value;
+    }
+
+}
