3a2b8c0fbac8ed968fd8d25514cacb00de468273
njozwiak <njozwiak@xebia.fr>
2013-12-12 13:04:41 +0100

    Add Montant

--- src/main/java/com/xebia/domain/Montant.java
+++ src/main/java/com/xebia/domain/Montant.java
@@ -1,10 +1,33 @@
 package com.xebia.domain;
 
-import java.math.BigDecimal;
+import com.xebia.domain.exception.MontantException;
 
 public class Montant {
 
-    private BigDecimal value;
+    private ProductDecimal value;
 
     private Currency currency;
+
+    public Montant(ProductDecimal value, Currency currency) {
+        this.value = value;
+        this.currency = currency;
+    }
+
+    public ProductDecimal getValue() {
+        return value;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public Montant add(Montant montant) throws MontantException {
+        if (this.currency.equals(montant.getCurrency())) {
+            ProductDecimal newValue = this.getValue().add(montant.getValue());
+            return new Montant(newValue, this.getCurrency());
+        } else {
+            throw new MontantException("cannot add montant with different currencies");
+        }
+
+    }
 }
