421152bdbfdd45adb29c6c629a8d641ff0495a3d
Gerome Egron <legunda.gg&gmail.com>
2014-01-15 23:43:11 +0100

    Add test

--- src/test/java/com/xebia/domain/product/ProductRepositoryIntegrationTest.java
+++ src/test/java/com/xebia/domain/product/ProductRepositoryIntegrationTest.java
@@ -2,8 +2,15 @@ package com.xebia.domain.product;
 
 import com.google.inject.Inject;
 import com.xebia.AbstractIntegrationTest;
+import com.xebia.domain.echeance.EcheanceRequest;
 import com.xebia.domain.product.ProductRepository;
+import org.fest.assertions.Assertions;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 public class ProductRepositoryIntegrationTest extends AbstractIntegrationTest {
 
@@ -11,7 +18,30 @@ public class ProductRepositoryIntegrationTest extends AbstractIntegrationTest {
     private ProductRepository productRepository;
 
     @Test
-    public void testFindOne() throws Exception {
+    public void testFindOne() {
 
     }
+
+    @Test
+    public void should_save_product() {
+        // Given
+        EcheanceRequest echeanceRequest = new EcheanceRequest();
+        echeanceRequest.setActive(true);
+        echeanceRequest.setBeginDate(new Date());
+        echeanceRequest.setEndDate(new Date());
+        echeanceRequest.setReoffer(BigDecimal.TEN);
+
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Save product test");
+        product.setMarketDate(new Date());
+        product.getEcheanceRequests().add(echeanceRequest);
+
+        // When
+        productRepository.save(product);
+
+        // Then
+        assertThat(productRepository.findAll()).isNotEmpty();
+    }
+
 }
