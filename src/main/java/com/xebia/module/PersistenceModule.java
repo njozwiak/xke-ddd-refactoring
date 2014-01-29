e80b74ab459068336cab861a5267cd68a241f9d4
njozwiak <njozwiak@xebia.fr>
2014-01-29 19:55:07 +0100

    Refactor package

--- src/main/java/com/xebia/module/PersistenceModule.java
+++ src/main/java/com/xebia/module/PersistenceModule.java
@@ -2,13 +2,11 @@ package com.xebia.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-
 import com.xebia.application.ProductApplicationService;
 import com.xebia.domain.comission.ComissionRepository;
 import com.xebia.domain.product.ProductRepository;
 import com.xebia.infrastructure.persistence.HibernateComissionRepository;
 import com.xebia.infrastructure.persistence.HibernateProductRepository;
-import com.xebia.repository.DeviseRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -23,7 +21,6 @@ public class PersistenceModule extends AbstractModule {
         entityManagerFactory = Persistence.createEntityManagerFactory("dddDB");
 
         bind(ComissionRepository.class).to(HibernateComissionRepository.class);
-        bind(DeviseRepository.class);
         bind(ProductRepository.class).to(HibernateProductRepository.class);
 
         bind(ProductApplicationService.class);
--- src/main/java/com/xebia/port/adapter/service/ProductDataService.java
+++ src/main/java/com/xebia/port/adapter/service/ProductDataService.java
@@ -5,7 +5,6 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.xebia.domain.ProductDecimal;
-import com.xebia.service.DataService;
 
 import java.math.BigDecimal;
 import java.util.Date;
