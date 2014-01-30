03a9a9a36165a7043eb5733168de2bc669beb853
njozwiak <njozwiak@xebia.fr>
2014-01-30 08:51:33 +0100

    Add test to echeanceToProduct

--- src/test/java/com/xebia/application/ProductApplicationServiceTest.java
+++ src/test/java/com/xebia/application/ProductApplicationServiceTest.java
@@ -1,22 +1,29 @@
 package com.xebia.application;
 
 import com.google.common.collect.Lists;
+
 import com.xebia.domain.ProductDecimal;
 import com.xebia.domain.currency.Currency;
 import com.xebia.domain.echeance.EcheanceRequest;
+import com.xebia.domain.echeance.EcheanceRequestBuilder;
 import com.xebia.domain.product.Product;
 import com.xebia.domain.product.ProductBuilder;
 import com.xebia.domain.product.ProductRepository;
 import com.xebia.port.adapter.service.ProductDataService;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ProductApplicationServiceTest {
 
     private ProductApplicationService productService;
@@ -49,6 +56,23 @@ public class ProductApplicationServiceTest {
         Boolean result = productService.containsFundingCurrencies(Lists.newArrayList(gpb));
 
         assertThat(result).isFalse();
+    }
+
+    @Test
+    public void should_add_echeance_to_product() {
+      // Given
+      Product product = new ProductBuilder().build();
+
+      EcheanceRequest echeance = new EcheanceRequestBuilder().build();
+
+      long productId = 1L;
+      when(productRepository.findOne(productId)).thenReturn(product);
+
+      // When
+      productService.addEcheanceToProduct(productId, echeance);
+      // Then
+
+      assertThat(product.getEcheanceRequestActive()).hasSize(1).contains(echeance);
     }
 
     @Test
