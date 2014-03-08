5b719103c8f760c6d52c87ec517662676252c088
njozwiak <njozwiak@xebia.fr>
2014-03-08 09:47:07 +0100

    Move Montant

---
+++ financial/src/main/java/com/xebia/domain/echeance/Montant.java
@@ -0,0 +1,37 @@
+package com.xebia.domain.echeance;
+
+import com.xebia.domain.currency.Currency;
+import com.xebia.domain.exception.MontantException;
+
+public class Montant {
+
+    private ProductDecimal value;
+
+    private Currency currency;
+
+    public Montant() {
+    }
+
+    public Montant(ProductDecimal value, Currency currency) {
+        this.value = value;
+        this.currency = currency;
+    }
+
+    public ProductDecimal getValue() {
+        return value;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public Montant add(Montant montant) throws MontantException {
+        if (this.currency.equals(montant.getCurrency())) {
+            ProductDecimal newValue = this.getValue().add(montant.getValue());
+            return new Montant(newValue, this.getCurrency());
+        } else {
+            throw new MontantException("cannot add montant with different currencies");
+        }
+
+    }
+}
